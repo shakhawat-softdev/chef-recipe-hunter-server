f30c5f297054350d52d7465c4440a4f670d92b02
Shakhawat <syedarafatcse@gmail.com>
2023-05-01 23:26:59 +0600

    basic installaitin done

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+var cors = require('cors')
+const app = express();
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+
+const chefs = require('./data/chefs.json');
+
+app.get('/', (req, res) => {
+   res.send('Chef recipe hunter is runnng')
+})
+
+app.get('/chefs', (req, res) => {
+   res.send(chefs);
+})
+
+app.listen(port, () => {
+   console.log(`Chef recipe hunter is runnng on port ${port}`)
+})
